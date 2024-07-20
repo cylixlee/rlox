e79105991f04ad6135664a89f6c5b83c53270568
Cylix Lee <cylix.lee@outlook.com>
2024-07-20 20:27:46 +0800

    Fixed PrepareInvoke issues.

--- crates/rlox-runtime/src/vm.rs
+++ crates/rlox-runtime/src/vm.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 use std::mem;
 use std::ops::Deref;
 use std::rc::Rc;
+use std::time::Instant;
 
 use rlox_intermediate::*;
 
@@ -11,34 +12,54 @@ use crate::value::Value;
 
 const STACK_SIZE: usize = 1024;
 
+type NativeFunction = fn(&mut VirtualMachine) -> Value;
+
 pub struct VirtualMachine {
     bytecode: Bytecode,
     program_count: usize,
+    stack_offset: usize,
     stack: Stack<Value, STACK_SIZE>,
     heap: Heap,
     globals: HashMap<String, Value>,
 
     // Invocation fields
+    native_functions: HashMap<String, NativeFunction>,
     chunks: Vec<Rc<Chunk>>,
     last_program_counts: Vec<usize>,
-    stack_offsets: Vec<usize>,
+    last_stack_offsets: Vec<usize>,
+    next_stack_offsets: Vec<usize>,
     return_value: Value,
+
+    // Timer
+    started: Instant,
+
+    // Stack Monitor
+    #[cfg(feature = "stack-monitor")]
+    call_stack: Vec<String>,
 }
 
 impl VirtualMachine {
     pub fn new(bytecode: Bytecode) -> Self {
         let chunk = Rc::clone(&bytecode.script);
         let exit_program_count = chunk.len();
+        let mut native_functions: HashMap<String, NativeFunction> = HashMap::new();
+        native_functions.insert(String::from("clock"), native_clock);
         Self {
             bytecode,
             program_count: 0,
+            stack_offset: 0,
             stack: Stack::new(),
             heap: Heap::new(),
             globals: HashMap::new(),
+            native_functions,
             chunks: vec![chunk],
             last_program_counts: vec![exit_program_count],
-            stack_offsets: vec![0],
+            last_stack_offsets: vec![0],
+            next_stack_offsets: Vec::new(),
             return_value: Value::Nil,
+            started: Instant::now(),
+            #[cfg(feature = "stack-monitor")]
+            call_stack: vec!["<script>".into()],
         }
     }
 
@@ -67,7 +88,13 @@ impl VirtualMachine {
                 }
 
                 #[cfg(feature = "stack-monitor")]
-                println!("{:04} {:?}", self.program_count, instruction);
+                {
+                    let function_name = self.call_stack.last().unwrap();
+                    println!(
+                        "{function_name}::{:04} {:?}",
+                        self.program_count, instruction
+                    );
+                }
 
                 match instruction {
                     Instruction::LoadConstant(index) => {
@@ -152,14 +179,12 @@ impl VirtualMachine {
                         *self.global_mut(name, span)? = value;
                     }
                     Instruction::GetLocal(index) => {
-                        let stack_offset = self.current_stack_offset();
                         self.stack
-                            .push(self.stack[stack_offset + index].clone(), span)?;
+                            .push(self.stack[self.stack_offset + index].clone(), span)?;
                     }
                     Instruction::SetLocal(index) => {
                         let value = self.stack.top(span)?.clone();
-                        let stack_offset = self.current_stack_offset();
-                        self.stack[stack_offset + index] = value;
+                        self.stack[self.stack_offset + index] = value;
                     }
                     Instruction::JumpIfFalse(offset) => {
                         let condition: bool = self.stack.top(span)?.boolean();
@@ -171,30 +196,45 @@ impl VirtualMachine {
                     Instruction::Jump(offset) => {
                         self.program_count = (self.program_count as isize + offset) as usize;
                     }
-                    Instruction::PrepareInvoke => self.stack_offsets.push(self.stack.len()),
+                    Instruction::PrepareInvoke => self.next_stack_offsets.push(self.stack.len()),
                     Instruction::Invoke => {
                         let name = self.pop_identifier(span.clone())?;
-                        let function = match self.bytecode.functions.get(&*name) {
-                            Some(function) => function,
-                            None => raise!("E0015", span),
-                        };
-                        let argument_count = self.stack.len() - self.current_stack_offset();
-                        if argument_count != function.arity {
-                            raise! {
-                                "E0016", span,
-                                format!(
-                                    "expected {} arguments, found {}",
-                                    function.arity, argument_count
-                                )
+                        if let Some(native_function) = self.native_functions.get(name.deref()) {
+                            self.next_stack_offsets.pop(); // Native functions don't need stack frames.
+                            let value = (*native_function)(self);
+                            self.stack.push(value, span)?;
+                        } else {
+                            let function = match self.bytecode.functions.get(name.deref()) {
+                                Some(function) => function,
+                                None => raise!("E0015", span),
+                            };
+
+                            #[cfg(feature = "stack-monitor")]
+                            self.call_stack.push(name.deref().clone());
+
+                            self.last_stack_offsets.push(self.stack_offset);
+                            self.stack_offset = self.next_stack_offsets.pop().unwrap();
+
+                            let argument_count = self.stack.len() - self.stack_offset;
+                            if argument_count != function.arity {
+                                raise! {
+                                    "E0016", span,
+                                    format!(
+                                        "expected {} arguments, found {}",
+                                        function.arity, argument_count
+                                    )
+                                }
                             }
+                            self.last_program_counts.push(self.program_count + 1);
+                            self.program_count = 0;
+                            self.chunks.push(Rc::clone(&function.chunk));
+                            self.return_value = Value::Nil;
+                            continue;
                         }
-                        self.last_program_counts.push(self.program_count + 1);
-                        self.program_count = 0;
-                        self.chunks.push(Rc::clone(&function.chunk));
-                        self.return_value = Value::Nil;
                     }
                     Instruction::Return => {
                         self.return_value = self.stack.pop(span)?;
+                        break;
                     }
                 }
 
@@ -206,10 +246,10 @@ impl VirtualMachine {
                 self.program_count += 1;
             }
 
-            let stack_offset = self.stack_offsets.pop().unwrap();
-            while self.stack.len() > stack_offset {
+            while self.stack.len() > self.stack_offset {
                 self.stack.try_pop().unwrap();
             }
+            self.stack_offset = self.last_stack_offsets.pop().unwrap();
             self.stack
                 .try_push(mem::replace(&mut self.return_value, Value::Nil));
             let last_program_count = self.last_program_counts.pop().unwrap();
@@ -225,10 +265,6 @@ impl VirtualMachine {
 
     fn current_chunk(&self) -> &Chunk {
         self.chunks.last().unwrap()
-    }
-
-    fn current_stack_offset(&self) -> usize {
-        *self.stack_offsets.last().unwrap()
     }
 
     fn pop_identifier(&mut self, span: Span) -> DiagnosableResult<Reference<String>> {
@@ -251,4 +287,8 @@ impl VirtualMachine {
         }
         raise!("E0012", span);
     }
+}
+
+fn native_clock(vm: &mut VirtualMachine) -> Value {
+    Value::Number(vm.started.elapsed().as_millis() as f64)
 }
